a0f618acf10ed8ecce35a90965dd114651d7fff7
Ryan Ramage <ryan.ramage@gmail.com>
2013-01-23 11:57:47 -0700

    Added adjustDensityGain for audio shadows.

--- index.js
+++ index.js
@@ -2,19 +2,30 @@
  * Thanks to the following sources of info
  * http://www.html5rocks.com/en/tutorials/webaudio/games/
  * https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html#PannerNode
-*/ 
+*/
 
 var init = false,
+    calculateAbsorption  = false,
+    audioInstances = [],
+    defaultDensity = 1,
+    densityMapping = [],
+    densityGainMin = 0.005,
     audioContext,
     game,
     audioDestination;
 
 
-exports.initGameAudio = function(game_) {
+exports.initGameAudio = function(game_, settings) {
 	audioContext = new webkitAudioContext();
 	audioDestination = audioContext.destination;
-	// Then render the camera on tick
 	game = game_;
+
+	if (!settings) settings = {};
+	if (settings.calculateAbsorption) calculateAbsorption = true;
+	if (settings.defaultDensity) defaultDensity = settings.defaultDensity;
+	if (settings.densityMapping) densityMapping = settings.densityMapping;
+	if (settings.densityGainMin) densityGainMin = settings.densityGainMin;
+
 	game.on('tick', tick);
 	init = true;
 };
@@ -35,16 +46,26 @@ exports.PositionAudio = function(options) {
 	if (options.url) self.initURL();
 	if (options.buffer) self.initBuffer();
 	if (options.source) self.initSource();
+	if (options.name) self.name = options.name;
+
+	// should add a distroy so not to leave things around....
+	audioInstances.push(self);
 };
 
 exports.PositionAudio.prototype.createPanner = function() {
 	var self = this;
 	self.panner = audioContext.createPanner();
+	self.gainNode   = audioContext.createGainNode();
 
 	var startingPosition = self.options.startingPosition;
 	if (!startingPosition || startingPosition.length !== 3) throw new Error('startingPosition required option. format: [x,y,z]');
 
 	self.panner.setPosition(startingPosition[0], startingPosition[1], startingPosition[2]);
+	self.position = {
+		x: startingPosition[0],
+		y: startingPosition[1],
+		z: startingPosition[2]
+	};
 	self.panner.coneOuterGain = self.options.coneOuterGain || Number(0);
 	self.panner.coneOuterAngle = self.options.coneOuterAngle || 360;
 	self.panner.coneInnerAngle = self.options.coneInnerAngle || 350;
@@ -77,12 +98,12 @@ exports.PositionAudio.prototype.initSource = function() {
 };
 
 
-
 exports.PositionAudio.prototype.play = function() {
 	var self = this;
 	if (!self.ready) throw new Error('Audio not ready. Did you call load?');
 
-	self.panner.connect(audioDestination);
+	self.gainNode.connect(audioDestination);
+	self.panner.connect(self.gainNode);
 	self.source.connect(self.panner);
 	if (self.source.noteOn) self.source.noteOn(0);
 	self.isPlaying = true;
@@ -128,4 +149,49 @@ function tick() {
 
 	// Set the orientation and the up-vector for the listener.
 	audioContext.listener.setOrientation(direction.x, direction.y, direction.z, up_direction.x, up_direction.y, up_direction.z);
+
+
+	if (!calculateAbsorption) return;
+	audioInstances.forEach(function(audio){
+		adjustDensityGain(audio, position);
+	});
 }
+
+// calculate absorption of things in the way. this is using the Beer-Lambert Law
+// see https://en.wikipedia.org/wiki/Beer%E2%80%93Lambert_law
+function adjustDensityGain(audio, position) {
+	var totalDistance = Math.sqrt( (Math.abs(audio.position.x - position.x))^2 + (Math.abs(audio.position.y - position.y))^2 + (Math.abs(audio.position.z - position.z))^2 );
+	var memo = {
+		audio: audio,
+		position: position,
+		totalDistance: totalDistance,
+		count: 0,
+		densitySum: 0,
+		totalCount: 0
+	};
+}
+
+function calculatePointDensity(pos, memo) {
+	memo.totalCount++;
+	var block = game.getBlock(pos);
+	if (block) {
+		memo.count++;
+		var density = densityMapping[block] || defaultDensity;
+		var block_distance_from_listener = Math.sqrt( (Math.abs(pos.x - memo.position.x))^2 + (Math.abs(pos.y - memo.position.y))^2 + (Math.abs(pos.z - memo.position.z))^2 );
+		var percent = block_distance_from_listener/memo.totalDistance;
+		memo.densitySum+= (((1.4*percent)-0.5)^2 + 0.5); // a u shape
+	}
+}
+
+function setDensityGain(memo) {
+	var gain = 1;
+	if (memo.count > 0) {
+		var exp = -1 * (memo.densitySum/120);
+		gain = Math.pow(10, exp);
+		if (gain < densityGainMin) gain = 0;
+		if (gain > 1) gain = 1;
+	}
+	memo.audio.gainNode.gain.value = gain;
+}
+
+
